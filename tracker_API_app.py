f64b36fe04fab42cda202073bc6413cbc1fb1e86
Ruchir Kafle <ruchirkafle@gmail.com>
2025-01-28 10:29:33 -0500

    working on tkintre

--- tracker_API_app.py
+++ tracker_API_app.py
@@ -57,56 +57,60 @@ window: tk.Tk = tk.Tk()
 window.geometry("350x350")
 window.title("Emoji Tracker App")
 
-welcome_label: tk.Label = tk.Label(window, text="Welcome to the Emoji Tracker!", anchor="w").grid(row=0, column=2, columnspan=3)
-description_label: tk.Label = tk.Label(window, text="This app lets you see and track emojis!", anchor="w").grid(row=1, column=2, columnspan=3)
+# grid is created implictily based on largest length of constituent elements
+# sticky allows 
+welcome_label: tk.Label = tk.Label(window, text="Welcome to the Emoji Tracker!").grid(row=0, column=0, columnspan=1, sticky="W")
+description_label: tk.Label = tk.Label(window, text="This app lets you see and track emojis!").grid(row=1, column=0, columnspan=1, sticky="W")
+
+input()
 
 # Main program logic
-while run:
-    user_input: str = input("What emoji would you like the information of? Please type in its name below, or all if you would like to see your options: \n").lower().strip()
-    print("\nLoading...")
+# while run:
+#     user_input: str = input("What emoji would you like the information of? Please type in its name below, or all if you would like to see your options: \n").lower().strip()
+#     print("\nLoading...")
 
-    # filter through every emoji's information and find the emoji that's name matches the user's input 
-    user_emoji: list | dict[str: str | list[str]] = list(filter(lambda list_item: 
-                                                           True if user_input == list_item["name"] 
-                                                           else True if user_input == "all"
-                                                           else False, emoji_data))
+#     # filter through every emoji's information and find the emoji that's name matches the user's input 
+#     user_emoji: list | dict[str: str | list[str]] = list(filter(lambda list_item: 
+#                                                            True if user_input == list_item["name"] 
+#                                                            else True if user_input == "all"
+#                                                            else False, emoji_data))
 
-    # error handling: if emoji isn't found, continue in loop
-    if not user_emoji:
-        print("That doesn't seem to be a valid emoji, please try again. \n")
-        continue
-    elif len(user_emoji) > 1:
-        for each_emoji in user_emoji:
-            print(each_emoji["name"])
+#     # error handling: if emoji isn't found, continue in loop
+#     if not user_emoji:
+#         print("That doesn't seem to be a valid emoji, please try again. \n")
+#         continue
+#     elif len(user_emoji) > 1:
+#         for each_emoji in user_emoji:
+#             print(each_emoji["name"])
         
-        print()
-        continue
+#         print()
+#         continue
     
-    # converting the emoji value, which is a list, to a dictionary
-    user_emoji = user_emoji[0]
+#     # converting the emoji value, which is a list, to a dictionary
+#     user_emoji = user_emoji[0]
     
-    # create an emoji object
-    emoji_obj: Emoji = create_emoji(user_emoji)
+#     # create an emoji object
+#     emoji_obj: Emoji = create_emoji(user_emoji)
     
-    # append the emoji to our list
-    emojis.append(emoji_obj)
+#     # append the emoji to our list
+#     emojis.append(emoji_obj)
 
-    # display emoji information
-    print(emoji_obj)
-    emoji_obj.display_info()
+#     # display emoji information
+#     print(emoji_obj)
+#     emoji_obj.display_info()
 
-    # while loop to keep asking the user if they would like to track another emoji if they keep entering invalid inputs
-    while True:
-        keep_going: str = input("\nTrack another emoji (y/n): \n").lower().strip()
+#     # while loop to keep asking the user if they would like to track another emoji if they keep entering invalid inputs
+#     while True:
+#         keep_going: str = input("\nTrack another emoji (y/n): \n").lower().strip()
 
-        if keep_going == "y":
-            break
-        elif keep_going == "n":
-            run = False
-            break
-        else:
-            print("\nThat doesn't seem to be a valid option, please try again.")
+#         if keep_going == "y":
+#             break
+#         elif keep_going == "n":
+#             run = False
+#             break
+#         else:
+#             print("\nThat doesn't seem to be a valid option, please try again.")
 
-# display summary to user
-print("\nHere's the emojis you looked at today!:")
-for emoji in emojis: print(f"{emoji.name}: {emoji}")
+# # display summary to user
+# print("\nHere's the emojis you looked at today!:")
+# for emoji in emojis: print(f"{emoji.name}: {emoji}")
