e2531f6980cfec9b92d5df956ecc4353aee3730d
Ruchir <ruchirkafle@gmail.com>
2025-01-28 22:46:36 -0500

    menu working for the most part

--- tracker_API_app.py
+++ tracker_API_app.py
@@ -12,15 +12,17 @@ class Emoji:
     
     # display info
     def display_info(self) -> None:
-        print(f"Name: {self.name}")
-        print(f"Group: {self.group}")
-        print(f"Category: {self.category}")
-        print(f"HTML Code: {self.html}")
-        print(f"Unicode: {self.unicode}")
+        character_label.configure(text=f"Character: {chr(int(self.unicode.removeprefix("U+"), 16))}")
 
-    # when printed, the instance of the class will return its unicode
-    def __str__(self) -> str:
-        return chr(int(self.unicode.removeprefix("U+"), 16))
+        name_label.configure(text=f"Name: {self.name}")
+
+        group_label.configure(text=f"Group: {self.group}")
+
+        category_label.configure(text=f"Category: {self.category}")
+        
+        html_label.configure(text=f"HTML Code: {self.html}")
+
+        html_label.configure(text=f"Unicode: {self.unicode}")
 
 # Function to fetch emoji data and return it
 def fetch_emoji_data() -> dict[str: str | list[str]] | None:
@@ -35,14 +37,37 @@ def fetch_emoji_data() -> dict[str: str | list[str]] | None:
         return None
     
 # Create a Emoji object using the data
-def create_emoji(emoji_json) -> Emoji:
+def create_emoji() -> Emoji:
+
+    user_input: str = entry.get().lower()
+
+    entry.delete(0, len(user_input))
+
+    # filter through every emoji's information and find the emoji that's name matches the user's input 
+    emoji_json: list | dict[str: str | list[str]] = list(filter(lambda list_item: 
+                                                           True if user_input == list_item["name"] 
+                                                           else True if user_input == "all"
+                                                           else False, 
+                                                           emoji_data))
+
+    # error handling: if emoji isn't found, continue in loop
+    if not emoji_json:
+        print("That doesn't seem to be a valid emoji, please try again. \n")
+    else:
+        # converting the emoji value, which is a list, to a dictionary
+        emoji_json = emoji_json[0]
+
     emoji: Emoji = Emoji(name=emoji_json["name"], 
                          group=emoji_json["group"], 
                          category=emoji_json["category"], 
                          html=emoji_json["htmlCode"][0], 
                          unicode=emoji_json["unicode"][0])
 
-    return emoji
+    # append the emoji to our list
+    emojis.append(emoji)
+
+    # display emoji information
+    emoji.display_info()
 
 # list of emojis the user tracks
 emojis: list[str] = []
@@ -58,59 +83,38 @@ window.geometry("350x350")
 window.title("Emoji Tracker App")
 
 # grid is created implictily based on largest length of constituent elements
-# sticky allows 
-welcome_label: tk.Label = tk.Label(window, text="Welcome to the Emoji Tracker!").grid(row=0, column=0, columnspan=1, sticky="W")
-description_label: tk.Label = tk.Label(window, text="This app lets you see and track emojis!").grid(row=1, column=0, columnspan=1, sticky="W")
+# sticky allows elements to always be aligned to a part of the cell
+welcome_label: tk.Label = tk.Label(window, text="Welcome to the Emoji Tracker!")
+welcome_label.grid(row=0, column=0, columnspan=2, sticky="W")
 
-input()
+description_label: tk.Label = tk.Label(window, text="This app lets you see and track emojis!")
+description_label.grid(row=1, column=0, columnspan=2, sticky="W")
 
-# Main program logic
-# while run:
-#     user_input: str = input("What emoji would you like the information of? Please type in its name below, or all if you would like to see your options: \n").lower().strip()
-#     print("\nLoading...")
+prompt_label: tk.Label = tk.Label(window, text="Enter an emoji's name:")
+prompt_label.grid(row=2, column=0, columnspan=1, sticky="W", pady=10)
 
-#     # filter through every emoji's information and find the emoji that's name matches the user's input 
-#     user_emoji: list | dict[str: str | list[str]] = list(filter(lambda list_item: 
-#                                                            True if user_input == list_item["name"] 
-#                                                            else True if user_input == "all"
-#                                                            else False, emoji_data))
+entry: tk.Entry = tk.Entry(window)
+entry.grid(row=2, column=1, columnspan=1, sticky="W", pady=10)
 
-#     # error handling: if emoji isn't found, continue in loop
-#     if not user_emoji:
-#         print("That doesn't seem to be a valid emoji, please try again. \n")
-#         continue
-#     elif len(user_emoji) > 1:
-#         for each_emoji in user_emoji:
-#             print(each_emoji["name"])
-        
-#         print()
-#         continue
-    
-#     # converting the emoji value, which is a list, to a dictionary
-#     user_emoji = user_emoji[0]
-    
-#     # create an emoji object
-#     emoji_obj: Emoji = create_emoji(user_emoji)
-    
-#     # append the emoji to our list
-#     emojis.append(emoji_obj)
+button: tk.Button = tk.Button(window, text="Find", command=create_emoji)
+button.grid(row=3, column=1, columnspan=1, sticky="W", pady=10)
 
-#     # display emoji information
-#     print(emoji_obj)
-#     emoji_obj.display_info()
+character_label: tk.Label = tk.Label(window, text=f"Character: ")
+character_label.grid(row=4, column=0, columnspan=1, sticky="W")
 
-#     # while loop to keep asking the user if they would like to track another emoji if they keep entering invalid inputs
-#     while True:
-#         keep_going: str = input("\nTrack another emoji (y/n): \n").lower().strip()
+name_label: tk.Label = tk.Label(window, text=f"Name: ")
+name_label.grid(row=5, column=0, columnspan=1, sticky="W")
 
-#         if keep_going == "y":
-#             break
-#         elif keep_going == "n":
-#             run = False
-#             break
-#         else:
-#             print("\nThat doesn't seem to be a valid option, please try again.")
+group_label: tk.Label = tk.Label(window, text=f"Group: ")
+group_label.grid(row=6, column=0, columnspan=1, sticky="W")
 
-# # display summary to user
-# print("\nHere's the emojis you looked at today!:")
-# for emoji in emojis: print(f"{emoji.name}: {emoji}")
+category_label: tk.Label = tk.Label(window, text=f"Category: ")
+category_label.grid(row=7, column=0, columnspan=1, sticky="W")
+
+html_label: tk.Label = tk.Label(window, text=f"HTML Code: ")
+html_label.grid(row=8, column=0, columnspan=1, sticky="W")
+
+html_label: tk.Label = tk.Label(window, text=f"Unicode: ")
+html_label.grid(row=9, column=0, columnspan=1, sticky="W")
+
+window.mainloop()
